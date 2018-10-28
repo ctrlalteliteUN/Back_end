97413fcc8cd1990df56119566f10fb9f9c8823b1
Cristian Laiton <cglaitong@unal.edu.co>
2018-10-28 15:12:09 -0500

    Autenticacion google comentada

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable
+
+  #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   #set score to N on create
   before_create :set_score
