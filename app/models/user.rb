38582cdf94735e7149dbcfbbdde12f5a0ad65ab8
Cristian Laiton <cglaitong@unal.edu.co>
2018-10-28 17:03:32 -0500

    omniauth google

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,10 +31,10 @@ class User < ApplicationRecord
   #devise :omniauthable, omniauth_providers: [:google_oauth2]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  #devise :database_authenticatable, :registerable,
+         #:recoverable, :rememberable, :validatable
 
-  #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   #set score to N on create
   before_create :set_score
