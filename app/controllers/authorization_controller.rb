0f2a4955798573ab20de81c18ddc4c0b6dd7f29c
David Santiago Gantiva Castro <dsgantivac@unal.edu.co>
2018-10-28 21:58:21 -0500

    pruebas1

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -17,17 +17,18 @@ include HTTParty
   def get_authorization
     client_id = "373142330185-hko54qc5fakooerj23p6n1494vj768h4.apps.googleusercontent.com"
     id_token = params["id_token"]
+    pecheo = "no se ejecuta"
     begin
       valid = Google::Auth::TokenValidator.new(id_token, client_id).validate
     rescue Google::Auth::TokenValidator::Error => e
-      puts e.message
+      pecheo = e.message
       valid = false
     end
 
     if valid
       render json: output = {'email' => 'la mama del crespo '}.to_json
     else
-      render json: output = {'email' => 'la puta madre no funciona '}.to_json
+      render json: output = {'email' => 'la puta madre no funciona' , 'error' => pecheo }.to_json
     end
   end
 
