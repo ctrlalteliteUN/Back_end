5e4997db14a4acaa2c5e345bbcf5af327ac831c3
Sergio <saayalac@unal.edu.co>
2018-10-28 22:41:14 -0500

    .

--- app/controllers/app_files_controller.rb
+++ app/controllers/app_files_controller.rb
@@ -28,7 +28,9 @@ class AppFilesController < ApplicationController
           #solo por id usuario
           file = AppFile.where("file_type_id= ? AND user_id = ?", params[:FileType], params[:user_id])
           if params[:stream].present?
-            send_data(Base64.decode64(file[0][:ruta]).force_encoding('BINARY'), :filename => file[0][:titulo])
+            ruta = file[0][:ruta]
+            ruta = ruta[(ruta.index(',')+1)..(ruta.lenght-1)]
+            send_data(Base64.decode64(ruta).force_encoding('BINARY'), :filename => file[0][:titulo])
           else
             render json: file
           end
