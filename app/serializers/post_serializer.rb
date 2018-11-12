f5475dc48fb98cf578c0a5bc295448488ebfa7b0
Cristian Laiton <cglaitong@unal.edu.co>
2018-11-11 19:28:57 -0500

    lat lnl serializador

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -25,7 +25,7 @@
 #
 
 class PostSerializer < ActiveModel::Serializer
-  attributes :id,:body,:solicitud,:title,:created_at,:updated_at,:user_id
+  attributes :id,:body,:solicitud,:title,:created_at,:updated_at,:user_id,:lat,:lng
   
   belongs_to :user
   belongs_to :group, required: false
