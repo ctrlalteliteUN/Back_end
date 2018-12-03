c9db4efa2d86622b39c87caeed8e3699673c7de6
Cristian Laiton <cglaitong@unal.edu.co>
2018-12-02 19:03:03 -0500

    fix services 2

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -11,7 +11,7 @@ class ServicesController < ApplicationController
       render json: $granted
     end
     #------------------------------------------------------------------------
-    @services = Service.all
+
     #Service.create( score: 3, post_id: 1)
     #@services = Service.create( score: 3, post_id: 1)
 
@@ -37,19 +37,19 @@ class ServicesController < ApplicationController
 
 
 
-    #--------------------------------------------------------------------------------------
-    @service = Service.new(service_params)
+      #--------------------------------------------------------------------------------------
+      @service = Service.new(service_params)
 
-    if @service.save
-      service_has_user = ServiceHasUser.new(@service[:id],params[:user_service_id])
-      if service_has_user.save
-        render json: @service, status: :created, location: @service
+      if @service.save
+        service_has_user = ServiceHasUser.new(@service[:id],params[:user_service_id])
+        if service_has_user.save
+          render json: @service, status: :created, location: @service
+        else
+          render json: @service.errors, status: :unprocessable_entity
+        end
       else
         render json: @service.errors, status: :unprocessable_entity
       end
-    else
-      render json: @service.errors, status: :unprocessable_entity
-    end
     else
       render json: $granted
     end
@@ -62,13 +62,13 @@ class ServicesController < ApplicationController
     if $granted
       $granted = false
 
-    #---------------------------------------------------------------------------------------
-    if @service.update(service_params)
-      render json: @service
-    else
-      render json: @service.errors, status: :unprocessable_entity
-    end
-    #-------------------------------------------------------------------------------------
+      #---------------------------------------------------------------------------------------
+      if @service.update(service_params)
+        render json: @service
+      else
+        render json: @service.errors, status: :unprocessable_entity
+      end
+      #-------------------------------------------------------------------------------------
     else
       render json: $granted
     end
@@ -85,13 +85,13 @@ class ServicesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_service
-      @service = Service.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_service
+    @service = Service.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def service_params
-      params.require(:service).permit(:score, :post_id)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def service_params
+    params.require(:service).permit(:score, :post_id)
+  end
 end
