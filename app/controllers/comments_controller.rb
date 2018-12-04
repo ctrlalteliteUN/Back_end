a417070dab44fa5ad61215507c5978f84a091e3b
David Santiago Gantiva Castro <dsgantivac@unal.edu.co>
2018-12-04 17:33:36 -0500

    token melo prro

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,46 +4,66 @@ class CommentsController < ApplicationController
 
   # GET /comments
   def index
-    @comments = Comment.where(post_id: params[:post_id])
-    #@comments = Comment.all
-    render json: @comments
+    if token_auth(request.headers["Authorization"],request.headers["ID"])
+      @comments = Comment.where(post_id: params[:post_id])
+      #@comments = Comment.all
+      render json: @comments
+    else
+      render json: false
+    end
   end
 
   # GET /comments/1
   def show
-    render json: @comment
+    if token_auth(request.headers["Authorization"],request.headers["ID"])
+      render json: @comment
+    else
+      render json: false
+    end
   end
 
   # POST /comments
   def create
-    @comment = Comment.new(comment_params)
-    #@comment.post_id = params[:post_id]
-    @comment.post = @post
+    if token_auth(request.headers["Authorization"],request.headers["ID"])
+      @comment = Comment.new(comment_params)
+      #@comment.post_id = params[:post_id]
+      @comment.post = @post
 
-    if @comment.save
-      #Notificar al dueño del post
-      usr = User.find(@post.user_id)
-      CommentMailer.with(user: usr,post: @post,commenter_name: User.find(comment_params[:user_id]).name,contenido:comment_params[:body]).CommmentCreated.deliver_later
-      #################################
-      render json: @comment, status: :created, location: @comment
+      if @comment.save
+        #Notificar al dueño del post
+        usr = User.find(@post.user_id)
+        CommentMailer.with(user: usr,post: @post,commenter_name: User.find(comment_params[:user_id]).name,contenido:comment_params[:body]).CommmentCreated.deliver_later
+        #################################
+        render json: @comment, status: :created, location: @comment
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: false
     end
   end
 
   # PATCH/PUT /comments/1
   def update
-    if @comment.update(comment_params)
-      render json: @comment
+    if token_auth(request.headers["Authorization"],request.headers["ID"])
+      if @comment.update(comment_params)
+        render json: @comment
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: false
     end
   end
 
   # DELETE /comments/1
   def destroy
-    CommentMailer.with(user: User.find(@comment.user_id),post: Post.find(@comment.post_id),reason:params[:reason]).Deleted.deliver_later
-    @comment.destroy
+    if token_auth(request.headers["Authorization"],request.headers["ID"])
+      CommentMailer.with(user: User.find(@comment.user_id),post: Post.find(@comment.post_id),reason:params[:reason]).Deleted.deliver_later
+      @comment.destroy
+    else
+      render json: false
+    end
   end
 
   private
