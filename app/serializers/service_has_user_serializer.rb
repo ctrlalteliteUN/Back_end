3e2334b4add515c7522d47243db2c26607413e06
Cristian Laiton <cglaitong@unal.edu.co>
2018-12-13 21:24:35 -0500

    fix serializador services

--- app/serializers/service_has_user_serializer.rb
+++ app/serializers/service_has_user_serializer.rb
@@ -21,7 +21,7 @@
 #
 
 class ServiceHasUserSerializer < ActiveModel::Serializer
-  attributes :id,:created_at,:updated_at,:service_id,:user_id
+  attributes :id,:created_at,:updated_at,:service_id,:user_id,:score
   
   belongs_to :user
   belongs_to :service
